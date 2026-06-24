79ce806ede692bf63b728e7ada472bd85bdd10b9
Cal Abel <crabel@mac.com>
2026-06-24 06:50:17 -0400

    implement PendSV GMAC callback model

--- cores/arduino/GMAC.cpp
+++ cores/arduino/GMAC.cpp
@@ -2,6 +2,7 @@
 
 #ifdef ETHERNET_HARDWARE_AVAILABLE
 #include <Arduino.h>
+#include <PendSV.h>
 
 namespace {
 constexpr uint32_t kMdioTimeoutMs = 10;
@@ -9,8 +10,61 @@ constexpr uint32_t kMdioWriteTen = 2;
 constexpr uint32_t kMdioReadOperation = 2;
 constexpr uint32_t kMdioWriteOperation = 1;
 
+struct GmacEventState {
+  volatile gmac::EventMask pendingEvents = gmac::EventNone;
+  gmac::EventCallback callback = nullptr;
+  void *callbackContext = nullptr;
+  bool serviceRegistered = false;
+};
+
+GmacEventState eventState;
+
 gmac_registers_t *gmacRegisters() {
   return reinterpret_cast<gmac_registers_t *>(GMAC_PERIPH);
+}
+
+uint32_t enterCritical() {
+  const uint32_t primask = __get_PRIMASK();
+  __disable_irq();
+  return primask;
+}
+
+void exitCritical(uint32_t primask) {
+  __set_PRIMASK(primask);
+}
+
+void gmacPendSvService(uint8_t serviceId, void *) {
+  if (serviceId != gmac::pendSvServiceId()) {
+    return;
+  }
+
+  gmac::EventCallback callback = nullptr;
+  void *callbackContext = nullptr;
+  gmac::EventMask events = gmac::EventNone;
+
+  const uint32_t primask = enterCritical();
+  events = eventState.pendingEvents;
+  eventState.pendingEvents = gmac::EventNone;
+  callback = eventState.callback;
+  callbackContext = eventState.callbackContext;
+  exitCritical(primask);
+
+  if (callback != nullptr && events != gmac::EventNone) {
+    callback(events, callbackContext);
+  }
+}
+
+bool ensurePendSvServiceRegistered() {
+  if (eventState.serviceRegistered) {
+    return true;
+  }
+
+  const bool registered = PendSV::instance().registerService(
+      gmac::pendSvServiceId(), gmacPendSvService);
+  if (registered) {
+    eventState.serviceRegistered = true;
+  }
+  return registered;
 }
 
 uint32_t mdcClockBits(uint32_t mckHz) {
@@ -41,6 +95,26 @@ bool waitManagementIdle() {
   } while ((millis() - startMs) < kMdioTimeoutMs);
 
   return false;
+}
+
+gmac::EventMask eventsFromInterruptStatus(uint32_t status) {
+  gmac::EventMask events = gmac::EventNone;
+
+  if ((status & (GMAC_ISR_RCOMP_Msk | GMAC_ISR_RXUBR_Msk)) != 0) {
+    events |= gmac::EventRxReady;
+  }
+  if ((status & (GMAC_ISR_TCOMP_Msk | GMAC_ISR_TXUBR_Msk)) != 0) {
+    events |= gmac::EventTxComplete;
+  }
+  if ((status & GMAC_ISR_MFS_Msk) != 0) {
+    events |= gmac::EventManagementComplete;
+  }
+  if ((status & (GMAC_ISR_TUR_Msk | GMAC_ISR_RLEX_Msk | GMAC_ISR_TFC_Msk |
+                 GMAC_ISR_ROVR_Msk | GMAC_ISR_HRESP_Msk)) != 0) {
+    events |= gmac::EventError;
+  }
+
+  return events;
 }
 } // namespace
 
@@ -74,6 +148,58 @@ bool gmac::beginManagement(uint32_t mckHz) {
 }
 
 bool gmac::isManagementIdle() { return waitManagementIdle(); }
+
+bool gmac::registerEventCallback(EventCallback callback, void *context) {
+  if (callback == nullptr) {
+    return false;
+  }
+  if (!ensurePendSvServiceRegistered()) {
+    return false;
+  }
+
+  const uint32_t primask = enterCritical();
+  eventState.callback = callback;
+  eventState.callbackContext = context;
+  exitCritical(primask);
+
+  return true;
+}
+
+void gmac::clearEventCallback() {
+  const uint32_t primask = enterCritical();
+  eventState.pendingEvents = EventNone;
+  eventState.callback = nullptr;
+  eventState.callbackContext = nullptr;
+  eventState.serviceRegistered = false;
+  exitCritical(primask);
+
+  PendSV::instance().clearService(pendSvServiceId());
+}
+
+gmac::EventMask gmac::pendingEvents() {
+  const uint32_t primask = enterCritical();
+  const EventMask events = eventState.pendingEvents;
+  exitCritical(primask);
+  return events;
+}
+
+void gmac::scheduleEvent(EventMask events) {
+  if (events == EventNone || !ensurePendSvServiceRegistered()) {
+    return;
+  }
+
+  const uint32_t primask = enterCritical();
+  eventState.pendingEvents |= events;
+  exitCritical(primask);
+
+  PendSV::instance().setPending(pendSvServiceId());
+}
+
+void gmac::handleInterrupt() {
+  gmac_registers_t *regs = gmacRegisters();
+  const uint32_t status = regs->GMAC_ISR & regs->GMAC_IMR;
+  scheduleEvent(eventsFromInterruptStatus(status));
+}
 
 void gmac::setMacAddress(const uint8_t mac[6]) {
   if (mac == nullptr) {
@@ -160,6 +286,10 @@ bool gmac::mdioWriteStart(uint8_t phyAddress, uint8_t registerAddress,
                    GMAC_MAN_WTN(kMdioWriteTen) | GMAC_MAN_DATA(value);
 
   return true;
+}
+
+extern "C" void GMAC_Handler(void) {
+  gmac::handleInterrupt();
 }
 
 #endif /* ETHERNET_HARDWARE_AVAILABLE */
--- cores/arduino/GMAC.h
+++ cores/arduino/GMAC.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "PendSV.h"
 #include "sam.h"
 
 #include <stdint.h>
@@ -12,13 +13,31 @@
 #ifdef ETHERNET_HARDWARE_AVAILABLE
 class gmac {
 public:
+  using EventMask = uint32_t;
+  using EventCallback = void (*)(EventMask events, void *context);
+
+  enum Event : EventMask {
+    EventNone = 0,
+    EventRxReady = 1u << 0,
+    EventTxComplete = 1u << 1,
+    EventManagementComplete = 1u << 2,
+    EventError = 1u << 3,
+  };
+
   static bool available();
   inline static uintptr_t baseAddress() { return GMAC_PERIPH; }
   static int irqNumber();
+  static constexpr uint8_t pendSvServiceId();
   static bool beginManagement(uint32_t mckHz = F_CPU);
   static bool isManagementIdle();
   static void setMacAddress(const uint8_t mac[6]);
   static void getMacAddress(uint8_t mac[6]);
+  static bool registerEventCallback(EventCallback callback,
+                                    void *context = nullptr);
+  static void clearEventCallback();
+  static EventMask pendingEvents();
+  static void scheduleEvent(EventMask events);
+  static void handleInterrupt();
   static bool mdioRead(uint8_t phyAddress, uint8_t registerAddress,
                        uint16_t *value);
   static bool mdioWrite(uint8_t phyAddress, uint8_t registerAddress,
@@ -26,4 +45,6 @@ public:
   static bool mdioWriteStart(uint8_t phyAddress, uint8_t registerAddress,
                              uint16_t value);
 };
+
+constexpr uint8_t gmac::pendSvServiceId() { return PendSVChannels::Gmac; }
 #endif /* ETHERNET_HARDWARE_AVAILABLE */
